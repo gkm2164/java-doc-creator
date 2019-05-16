5e66a7867658ea714d09c679e5f5b894a7fc63df
Ben. Go <ben.go@kakaocorp.com>
2019-05-16 19:18:51 +0900

    update

--- src/main/scala/co/gyeongmin/scala/godoc/javadoc/LRParser.scala
+++ src/main/scala/co/gyeongmin/scala/godoc/javadoc/LRParser.scala
@@ -5,65 +5,6 @@ import co.gyeongmin.scala.godoc.javadoc.LRParser._
 
 import scala.annotation.tailrec
 
-object LRParserRunner {
-  @tailrec
-  def matchStack(tokens: List[LRToken], stack: List[LRSymbol]): String = {
-    println(s"tokens: ${tokens.mkString("[", ", ", "]")}," +
-      s" stack: ${stack.mkString("[", ", ", "]")}")
-    (tokens, stack) match {
-      case (_, E(y) :: (op@(Plus | Minus)) :: E(x) :: t) =>
-        println("reduce => E -> E (+ | -) E")
-        matchStack(tokens, E(s"($x ${op.value} $y)") :: t)
-
-      case (op :: _, E(v) :: t) if !op.isOneOf(Plus, Minus) =>
-        println(s"reduce => C -> E (lookahead 1 token, $op")
-        matchStack(tokens, C(v) :: t)
-
-      case (_, T(y) :: (op@(Mult | Div)) :: T(x) :: t) =>
-        println("reduce => T -> T (* | /) T")
-        matchStack(tokens, T(s"($x ${op.value} $y)") :: t)
-
-      case (op :: _, T(v) :: t) if !op.isOneOf(Mult, Div) =>
-        println(s"reduce => E -> T (lookahead 1 token, $op)")
-        matchStack(tokens, E(v) :: t)
-
-      case (_, F(v) :: Not :: t) =>
-        println(s"reduce => F -> ! F")
-        matchStack(tokens, F(s"!$v") :: t)
-
-      case (_, F(v) :: t) =>
-        println("reduce => T -> F")
-        matchStack(tokens, T(v) :: t)
-
-      case (_, Token(v) :: t) =>
-        println(s"""reduce => F -> Token("$v")""")
-        matchStack(tokens, F(v) :: t)
-
-      case (_, RBracket :: E(x) :: LBracket :: t) =>
-        println("reduce => F -> ( E )")
-        matchStack(tokens, F(s"$x ") :: t)
-
-      case (_, C(y) :: (op: CondOp) :: C(x) :: t) =>
-        println("reduce => C -> C (==, ||, &&, ...) C")
-        matchStack(tokens, C(s"($x ${op.value} $y)") :: t)
-
-      case (op :: _, C(v) :: t) if !op.isOneOf(Equal, NotEqual, Less, LessEqual, Greater, GreaterEqual, Or, And) =>
-        println(s"reduce => S -> C (lookahead 1 token, $op)")
-        matchStack(tokens, S(v) :: t)
-
-      case (_, Semicolon :: S(v) :: t) => println("reduce => S -> S ;"); matchStack(tokens, S(v) :: t)
-
-      case (Nil, S(v) :: Nil) => v
-
-      case _ => tokens match {
-        case Nil => throw new TokenNotAcceptedException(s"error, $stack")
-        case h :: t => println("shift"); matchStack(t, h :: stack)
-      }
-    }
-  }
-}
-
-
 object LRParser {
 
   def main(args: Array[String]): Unit = {
@@ -173,4 +114,62 @@ object LRParser {
     override def value: String = ";"
   }
 
+}
+
+object LRParserRunner {
+  @tailrec
+  def matchStack(tokens: List[LRToken], stack: List[LRSymbol]): String = {
+    println(s"tokens: ${tokens.mkString("[", ", ", "]")}," +
+      s" stack: ${stack.mkString("[", ", ", "]")}")
+    (tokens, stack) match {
+      case (_, E(y) :: (op@(Plus | Minus)) :: E(x) :: t) =>
+        println("reduce => E -> E (+ | -) E")
+        matchStack(tokens, E(s"($x ${op.value} $y)") :: t)
+
+      case (op :: _, E(v) :: t) if !op.isOneOf(Plus, Minus) =>
+        println(s"reduce => C -> E (lookahead 1 token, $op")
+        matchStack(tokens, C(v) :: t)
+
+      case (_, T(y) :: (op@(Mult | Div)) :: T(x) :: t) =>
+        println("reduce => T -> T (* | /) T")
+        matchStack(tokens, T(s"($x ${op.value} $y)") :: t)
+
+      case (op :: _, T(v) :: t) if !op.isOneOf(Mult, Div) =>
+        println(s"reduce => E -> T (lookahead 1 token, $op)")
+        matchStack(tokens, E(v) :: t)
+
+      case (_, F(v) :: Not :: t) =>
+        println(s"reduce => F -> ! F")
+        matchStack(tokens, F(s"!$v") :: t)
+
+      case (_, F(v) :: t) =>
+        println("reduce => T -> F")
+        matchStack(tokens, T(v) :: t)
+
+      case (_, Token(v) :: t) =>
+        println(s"""reduce => F -> Token("$v")""")
+        matchStack(tokens, F(v) :: t)
+
+      case (_, RBracket :: E(x) :: LBracket :: t) =>
+        println("reduce => F -> ( E )")
+        matchStack(tokens, F(s"$x ") :: t)
+
+      case (_, C(y) :: (op: CondOp) :: C(x) :: t) =>
+        println("reduce => C -> C (==, ||, &&, ...) C")
+        matchStack(tokens, C(s"($x ${op.value} $y)") :: t)
+
+      case (op :: _, C(v) :: t) if !op.isOneOf(Equal, NotEqual, Less, LessEqual, Greater, GreaterEqual, Or, And) =>
+        println(s"reduce => S -> C (lookahead 1 token, $op)")
+        matchStack(tokens, S(v) :: t)
+
+      case (_, Semicolon :: S(v) :: t) => println("reduce => S -> S ;"); matchStack(tokens, S(v) :: t)
+
+      case (Nil, S(v) :: Nil) => v
+
+      case _ => tokens match {
+        case Nil => throw new TokenNotAcceptedException(s"error, $stack")
+        case h :: t => println("shift"); matchStack(t, h :: stack)
+      }
+    }
+  }
 }
