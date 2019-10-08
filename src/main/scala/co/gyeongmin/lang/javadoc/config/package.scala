5c4dd843902ea730bdf0bda388a865d838c19417
Ben. Go <ben.go@kakaocorp.com>
2019-10-08 18:24:54 +0900

    update

--- src/main/scala/co/gyeongmin/lang/javadoc/config/package.scala
+++ src/main/scala/co/gyeongmin/lang/javadoc/config/package.scala
@@ -14,46 +14,66 @@ package object config {
       thisValue = Option(value)
     }
 
-    def bind(x: Target, v: T): Target
+    def binder(x: Target, v: T): Target
+
+    def bind(x: Target): Target = binder(x, getOrDefault)
 
     def getOrDefault: T = thisValue.getOrElse(defaultValue)
   }
 
+
   implicit def partialSettingKeyImplicit[Target]: Monad[SettingKey[_, Target]] = new Monad[SettingKey[*, Target]] {
     override def flatMap[A, B](fa: SettingKey[A, Target])(f: A => SettingKey[B, Target]): SettingKey[B, Target] = new SettingKey[B, Target] {
-      private val nextSetter = f(fa.getOrDefault)
+      private val thisValue = fa.getOrDefault
+      private val nextSetter = f(thisValue)
 
-      override def defaultValue: B = f(fa.getOrDefault).getOrDefault
+      override def defaultValue: B = nextSetter.getOrDefault
 
-      override def bind(x: Target, v: B): Target = nextSetter.bind(fa.bind(x, fa.getOrDefault), getOrDefault)
+      override def binder(x: Target, v: B): Target = nextSetter.binder(fa.binder(x, thisValue), v)
     }
 
     @scala.annotation.tailrec
     override def tailRecM[A, B](a: A)(f: A => SettingKey[Either[A, B], Target]): SettingKey[B, Target] =
       f(a).getOrDefault match {
-      case Left(v) => tailRecM(v)(f)
-      case Right(v) => pure(v)
-    }
+        case Left(v) => tailRecM(v)(f)
+        case Right(v) => pure(v)
+      }
 
     override def pure[A](v: A): SettingKey[A, Target] = SettingKey((x, _) => x, v)
   }
 
-  implicit class DescriptionBuilderClass[T](self: T) {
+  implicit class SettingKeyMonadSyntax[A, Target](fa: SettingKey[A, Target]) {
+    private val F = implicitly[Monad[SettingKey[*, Target]]]
+
+    def flatMap[B](f: A => SettingKey[B, Target]): SettingKey[B, Target] = F.flatMap(fa)(f)
+
+    def map[B](f: A => B): SettingKey[B, Target] = F.map(fa)(f)
+  }
+
+  trait BuilderType[T] {
+    final def realize: T = combine.foldRight(empty)(_.bind(_))
+
+    def empty: T
+
+    def combine: List[SettingKey[_, T]]
+  }
+
+  trait DescriptorType
+
+  implicit class DescriptionBuilderClass[T <: DescriptorType](self: T) {
     def set(f: T => Unit): T = {
       f(self)
       self
     }
   }
 
-  implicit class SettingKeyMonadSyntax[A, Target](fa: SettingKey[A, Target]) {
-    private val F = implicitly[Monad[SettingKey[*, Target]]]
-    def flatMap[B](f: A => SettingKey[B, Target]): SettingKey[B, Target] = F.flatMap(fa)(f)
-    def map[B](f: A => B): SettingKey[B, Target] = F.map(fa)(f)
-  }
+  case class DocumentDescription(baseDir: String, outputDir: String, description: String) extends DescriptorType
 
-  case class DocumentDescription(baseDir: String, outputDir: String, description: String)
+  case class DebugOption(stackTrace: Boolean,
+                         maxStackSize: Int,
+                         printOnlyAccepted: Boolean) extends DescriptorType
 
-  class DocumentDescriptionBuilder {
+  class DocumentDescriptionBuilder extends BuilderType[DocumentDescription] {
     val baseDir: SettingKey[String, DocumentDescription] =
       SettingKey((self, value) => self.copy(baseDir = value), ".")
     val outputDir: SettingKey[String, DocumentDescription] =
@@ -62,47 +82,26 @@ package object config {
       SettingKey((self, value) => self.copy(description = value), "default description")
     val empty = DocumentDescription("", "", "")
 
-    def realize: DocumentDescription = combine.bind(empty, empty)
-
-    private def combine: SettingKey[DocumentDescription, DocumentDescription] = for {
-      baseDir <- this.baseDir
-      outputDir <- this.outputDir
-      description <- this.description
-    } yield DocumentDescription(baseDir, outputDir, description)
+    val combine: List[SettingKey[_, DocumentDescription]] = baseDir :: outputDir :: description :: Nil
   }
 
-  case class DebugOption(stackTrace: Boolean,
-                         maxStackSize: Int,
-                         printOnlyAccepted: Boolean)
-
-  class DebugOptionBuilder {
+  class DebugOptionBuilder extends BuilderType[DebugOption] {
     val stackTrace: SettingKey[Boolean, DebugOption] =
       SettingKey((self, value) => self.copy(stackTrace = value), false)
     val maxStackSize: SettingKey[Int, DebugOption] =
       SettingKey((self, value) => self.copy(maxStackSize = value), 1)
     val printOnlyAccepted: SettingKey[Boolean, DebugOption] =
       SettingKey((self, value) => self.copy(printOnlyAccepted = value), false)
+
     val empty = DebugOption(stackTrace = false, 0, printOnlyAccepted = false)
-
-    def realize: DebugOption = combine.bind(empty, empty)
-
-    private def combine: SettingKey[DebugOption, DebugOption] = for {
-      stackTrace <- this.stackTrace
-      maxStackSize <- this.maxStackSize
-      printOnlyAccepted <- this.printOnlyAccepted
-    } yield DebugOption(stackTrace, maxStackSize, printOnlyAccepted)
+    val combine: List[SettingKey[_, DebugOption]] = stackTrace :: maxStackSize :: printOnlyAccepted :: Nil
   }
 
   object SettingKey {
-    def apply[T, Target](binder: (Target, T) => Target, givenDefaultValue: T): SettingKey[T, Target] = new SettingKey[T, Target] {
+    def apply[T, Target](givenBinder: (Target, T) => Target, givenDefaultValue: T): SettingKey[T, Target] = new SettingKey[T, Target] {
       override def defaultValue: T = givenDefaultValue
 
-      override def bind(x: Target, v: T): Target = binder(x, v)
+      override def binder(x: Target, v: T): Target = givenBinder(x, v)
     }
   }
-
-  object DocumentDescriptionBuilder {
-    def apply(): DocumentDescriptionBuilder = new DocumentDescriptionBuilder
-  }
-
 }
